3e5a6b61d615534ac65adde04a7e42a86f8b06b4
Dallan Reis <dallanaugusto@gmail.com>
2021-10-31 13:40:23 -0300

    CRUD de Categoria (POST, PUT e DELETE) Parte 1 e 2 tratamento da exceção

--- src/main/java/com/dallan/petshop/resources/CategoriaResource.java
+++ src/main/java/com/dallan/petshop/resources/CategoriaResource.java
@@ -1,11 +1,15 @@
 package com.dallan.petshop.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.dallan.petshop.domain.Categoria;
 import com.dallan.petshop.services.CategoriaService;
@@ -21,8 +25,30 @@ public class CategoriaResource {
 	public ResponseEntity<Categoria> find(@PathVariable Integer id) {
 
 		Categoria obj = service.find(id);
-
 		return ResponseEntity.ok().body(obj);
+	}
+
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Categoria> insert(@RequestBody Categoria obj) {
+
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+	public ResponseEntity<Categoria> update(@RequestBody Categoria obj, @PathVariable Integer id) {
+		Categoria found = service.find(id);
+		found.setNome(obj.getNome());
+
+		found = service.update(found);
+		return ResponseEntity.noContent().build();
+	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<Categoria> delete(@PathVariable Integer id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 
 }
--- src/main/java/com/dallan/petshop/services/CategoriaService.java
+++ src/main/java/com/dallan/petshop/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.dallan.petshop.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.dallan.petshop.domain.Categoria;
 import com.dallan.petshop.repositories.CategoriaRepository;
+import com.dallan.petshop.services.exceptions.DataIntegrityException;
 import com.dallan.petshop.services.exceptions.ObjetoNaoEncontradoException;
 
 @Service
@@ -18,6 +20,25 @@ public class CategoriaService {
 	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjetoNaoEncontradoException(id, Categoria.class));
+	}
+
+	public Categoria insert(Categoria obj) {
+		obj.setId(null);
+		return repo.save(obj);
+	}
+
+	public Categoria update(Categoria obj) {
+		find(obj.getId());
+		return repo.save(obj);
+	}
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Categoria possui produtos. Não é possível removê-la");
+		}
 	}
 
 }
